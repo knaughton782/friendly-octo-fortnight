4c7164b68b996405f02f0e4f6f9b767c32d87346
Kirsten Naughton <kirsten.naughton@gmail.com>
2020-02-05 17:36:03 -0700

    changed for loop over array to map function

--- src/index.js
+++ src/index.js
@@ -5,25 +5,19 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
+//use the map function to render data from the array
 
 const App = props => {
+    // for loop to push items to new array
+    const items = ['bread', 'milk', 'eggs', 'chocolate', 'chocolate', 'chocolate'];
     
-    return <List items={listItems} />
+    return <List items={items} />
 }
 
-// for loop to push items to new array
-const items = ['bread', 'milk', 'eggs', 'chocolate'];
-
-const listItems = [];
-
-for(let i = 0; i < items.length; i++) {
-    
-    listItems.unshift(<li key={i}>{items[i]}</li>); // using unshift to add them in backwards so I can see on the html page that is its the listItems array
-    // each item needs a key to indicate the number and array location, The key attribute is used by React to identify which items have been changed or removed, as well as when they're added.
+const List = props => {
+    const listItems = props.items.map((item, index) => <li key={index}>{item}</li>);
+    return <ul>{listItems}</ul>
 }
-
-const List = props => <ul>{props.items}</ul>;
 
 
 ReactDOM.render(

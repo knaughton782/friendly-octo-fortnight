907470187e6d0255243c3cbd067a247d031f4802
Kirsten Naughton <kirsten.naughton@gmail.com>
2020-02-17 17:03:13 -0700

    first stateful component example, lesson 4.2

--- src/index.js
+++ src/index.js
@@ -2,31 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
+// import * as serviceWorker from './serviceWorker';
 // import App from './App';
 
-import * as serviceWorker from './serviceWorker';
-
 const App = props => {
-    const cars = [
-        'Tacoma',
-        'Suburban',
-        'Jeep',
-        'Ford Explorer'
-    ];
-    return <List cars={cars}/>
+    return (
+        <div>
+        <Hello name='Nicki' />
+        </div>
+    )
+}
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Hello, {this.props.name}!</h1>
+            </div>
+        )
+    }
 }
 
-const List = props => {
-    const listItems = props.cars.map((item, index) => (
-        <li key={index}>{item}</li>
-    ));
-    return <ul>{listItems}</ul>
-}
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
 
-serviceWorker.unregister();
+//serviceWorker.unregister();

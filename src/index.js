584ee886648d4a8388f8a6b375338788482b4ea0
Kirsten Naughton <kirsten.naughton@gmail.com>
2020-02-19 15:45:28 -0700

    practice the update function for setState

--- src/index.js
+++ src/index.js
@@ -5,25 +5,36 @@ import './index.css';
 // import * as serviceWorker from './serviceWorker';
 // import App from './App';
 
-const App = props => {
-    return (
-        <div>
-        <Hello name='Nicki' />
-        </div>
-    )
-}
-class Hello extends React.Component {
+class Text extends React.Component {
+    constructor() {
+        //won't use props in this component because state is managing everything
+        super(); //creates an instance of the base class called component
+        //super is required before you can set the state of a component
+        this.state = {
+            text: 'don\'t like Steve whatshisname'
+            //this is the default state
+        }
+    }
+    update(event) {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
     render() {
+        //jsx to render to the dom
         return (
             <div>
-            <h1>Hello, {this.props.name}!</h1>
+                <input style={{ margin: 100 + 'px' }} type="text" onChange={this.update.bind(this)} />
+                <h1 style={{ margin: 100 + 'px' }}>{this.state.text}</h1>
             </div>
+            //bind is required so it stays into this local scope
         )
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
 
+ReactDOM.render(<Text />, document.getElementById('root'));
 
 
 //serviceWorker.unregister();

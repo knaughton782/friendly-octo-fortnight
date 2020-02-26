79b7c6c14f1534e9c492b95105c5c76032afbd64
Kirsten Naughton <kirsten.naughton@gmail.com>
2020-02-25 21:16:21 -0700

    updated content, added footer, added styles for footer and header

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NumberOfStudents from './index.js';
 import Header from './Header.js';
+import Footer from './Footer.js'
 
 const App = props => {
   return (
     <div>
       <Header />
       <NumberOfStudents />
+      <Footer />
     </div>
 
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/index.css';
+
+const Footer = (props) => (
+    <div className='footer'>
+
+        <h4 className='center'>Created With React</h4>
+
+
+    </div>
+
+)
+
+export default Footer;

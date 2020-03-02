5bcfe06a3a722281d7b1d4cc433a232ed2206aa3
Kirsten Naughton <kirsten.naughton@gmail.com>
2020-03-02 14:09:13 -0700

    updated with a sub routing example on one of the pages

--- src/views/Topics.js
+++ src/views/Topics.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Topic from '../components/Topic';
+import { Route, Link } from 'react-router-dom';
 
 
-const Topics = () => (
+const Topics = ({ match }) => (
     <div>
         <Header />
+
         <h1 className='center'>Topics</h1>
+        <Route path={`${match.url}/:topicId`} component={Topic} />
+        <Route exact path={match.url} render={() => <h3>Please select a topic:</h3>} />
+        <ul>
+            <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
+            <li><Link to={`${match.url}/rendering`}>Components</Link></li>
+            <li><Link to={`${match.url}/rendering`}>Props vs State</Link></li>
+        </ul>
         <Footer />
     </div>
 
